d24341c604d473bd7aa9f89bd74373b0baeaceb0
Sabareeswaran <94094997+sabareeswarans11@users.noreply.github.com>
2022-11-01 12:37:37 -0400

    Added test cases

---
+++ data2.asm
@@ -0,0 +1,9 @@
+	.data
+	.word	14
+	.word	15
+	.word	16
+	.word	17
+	.word	2147483646
+	.word	20
+	.word	21
+	.word	-2147483646
---
+++ directive.asm
@@ -0,0 +1,8 @@
+	.data
+	.word	14
+	.word	15
+	.word	16
+	.word	17
+	.text
+	add	$3,$1,$2
+        
